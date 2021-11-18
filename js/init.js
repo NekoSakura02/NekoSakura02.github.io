c06972d4baaaa6fb39541f22ba7eb471a321e5bb
NekoSakura023cv1 <dashix2010@gmail.com>
2021-11-18 14:34:11 -0600

    Conexión firebas

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyAQpF9EhhLG-2dAvuMGlRNRPmjYo-AsYhY",
-    authDomain: "dbweb-836ad.firebaseapp.com",
-    projectId: "dbweb-836ad",
-    storageBucket: "dbweb-836ad.appspot.com",
-    messagingSenderId: "760689440403",
-    appId: "1:760689440403:web:216c6084530d0780e97f66",
-    measurementId: "G-Q3S23JE30Y"
-      });
+  apiKey: "AIzaSyAQpF9EhhLG-2dAvuMGlRNRPmjYo-AsYhY",
+  authDomain: "dbweb-836ad.firebaseapp.com",
+  projectId: "dbweb-836ad",
+  storageBucket: "dbweb-836ad.appspot.com",
+  messagingSenderId: "760689440403",
+  appId: "1:760689440403:web:216c6084530d0780e97f66",
+  measurementId: "G-Q3S23JE30Y"
+    });
