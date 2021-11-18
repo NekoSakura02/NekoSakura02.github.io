87bdb3ebd84408fbbf165b8facc133e7af7e0e1b
NekoSakura023cv1 <dashix2010@gmail.com>
2021-11-18 14:30:01 -0600

    Agrega proyecto de autenticación

---
+++ README.md
@@ -0,0 +1,5 @@
+# NekoSakura023cv1.github.io
+Ejemplo de un sitio web, un chat y acceso a Firebase (Firestore con Google)
+
+La URL de este sitio:
+https://NekoSakura023cv1.github.io
---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAQpF9EhhLG-2dAvuMGlRNRPmjYo-AsYhY",
+    authDomain: "dbweb-836ad.firebaseapp.com",
+    projectId: "dbweb-836ad",
+    storageBucket: "dbweb-836ad.appspot.com",
+    messagingSenderId: "760689440403",
+    appId: "1:760689440403:web:216c6084530d0780e97f66",
+    measurementId: "G-Q3S23JE30Y"
+      });
